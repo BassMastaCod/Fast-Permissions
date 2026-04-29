40e58568db322a1bdf22854dbd76ac40adf6ac2f
Cody M Sommer <BassMastaCod@gmail.com>
2026-04-29 11:30:36 -0400

    Refactor page decorator to use `_register_page` method
    Simplifies the restricted page registration by delegating logic to the new `_register_page` method. Removes redundant `response_wrapper` and unused `HTMLResponse` import for cleaner and more maintainable code.

--- fast_permissions/pwa.py
+++ fast_permissions/pwa.py
@@ -4,7 +4,6 @@ from urllib.parse import quote
 
 from daomodel.db import init_db, create_engine
 from fastapi import Request, Depends, HTTPException, status
-from fastapi.responses import HTMLResponse
 
 from fast_permissions import RestrictedController
 from fast_permissions.html import login_template
@@ -115,23 +114,11 @@ class PWAWithAuth(PWA):
                         unauthorized_redirect: Optional[str] = None,
                         **get_kwargs):
         """Decorator that creates a page requiring authentication."""
-        route = self.with_prefix(route)
         url = unauthorized_redirect or self.unauthorized_redirect
         get_user = self.get_current_user_with_redirect(url) if url else self.get_current_user
 
         def decorator(func):
-            async def response_wrapper(request: Request, context: dict = Depends(func), user: User = Depends(get_user)):
-                return HTMLResponse(self.page_template.render(
-                    path_prefix=self.prefix,
-                    request=request,
-                    title=context.get('title', self.title),
-                    color=color,
-                    css=ensure_list(css) + self.global_css,
-                    js=ensure_list(js) + self.global_js,
-                    js_libraries=ensure_list(js_libraries),
-                    body=self.env.get_template(html).render(**context)
-                ))
-            self.get(route, include_in_schema=False, **get_kwargs)(response_wrapper)
-            logger.info(f'Registered restricted page at {route}')
+            logger.info(f'Registering restricted page...')
+            self._register_page(route, html, css, js, js_libraries, color, Depends(get_user), func=func, **get_kwargs)
             return func
         return decorator
